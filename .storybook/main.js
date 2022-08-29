905df9320b03028c0157fdaea670dc523d4dd501
Josh Wilkerson <jgwilkerson@gmail.com>
2022-08-29 11:04:54 -0400

    storybook: fix postCSS deprication warning

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,16 +8,19 @@ module.exports = {
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-postcss"
+  ],
+  plugins: [
+    require('autoprefixer')
   ],
   framework: "@storybook/react",
   webpackFinal: async (config, { configType }) => {
-    // Make whatever fine-grained changes you need
     config.module.rules.push({
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"],
       include: path.resolve(__dirname, "../"),
-    });
+    })
 
     // Return the altered config
     return config
