ea36a9e386ed84602572eda0423e181033e056f8
Josh Wilkerson <jgwilkerson@gmail.com>
2022-10-18 06:54:05 -0400

    components: update Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,30 +1,40 @@
 import React from 'react'
 
 export interface ButtonProps {
-  variant?: 'filled' | "outline" | "naked"
-  /**
-   * How large should the button be?
-   */
-  size?: 'lg' | 'md' | 'sm' | 'xs'
+  variant?: "filled" | "outline" | "naked"
+  theme?: "primary" | "destroy" | "alert"
+  size?: "lg" | "md" | "sm" | "xs"
   label: string
   disabled: boolean
   onClick: () => void
 }
 
-/**
- * Primary UI component for user interaction
-*/
 export const Button: React.FC<ButtonProps> = ({
   size = 'lg',
   label = "",
-  variant = 'filled',
+  theme = "primary",
+  variant = "filled",
   disabled = false,
   ...props
 }) => {
+  const getVariantClass = () => {
+    if (variant == "naked") {
+      return "naked-btn"
+    } else if (variant == "outline") {
+      return "secondary-btn"
+    } else {
+      return "filled-btn"
+    }
+  }
   return (
     <button
       type="button"
-      className={['btn', `${size}-btn`, `${variant}-btn`].join(' ')}
+      className={[
+        "btn",
+        `${size}-btn`,
+        `${getVariantClass()}`,
+        `${theme}-btn`,
+      ].join(" ")}
       disabled={disabled}
       {...props}
     >
